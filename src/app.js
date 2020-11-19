88760a93141d5518f7bd8a2d785c57aabd441746
Paulo Leite Costa <pauloleitecosta14@gmail.com>
2020-11-18 22:43:36 -0300

    Update port env

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsDirectoryPath = path.join(__dirname, '../templates/views')
@@ -75,15 +77,6 @@ app.get('/weather', (req, res) => {
     
 })
 
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'Yout must provide a search term'
-        })
-    }
-    console.log(req.query)
-    res.send({ products: [] })
-})
 
 app.get('*', (req, res) => {
     res.render('404', {
@@ -91,6 +84,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server on port 3000")
+app.listen(port, () => {
+    console.log(`Server on port ${port}`)
 })
