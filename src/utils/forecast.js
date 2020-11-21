cd672ee4e6ab77608132dd6245eb1b82db38879b
Paulo Leite Costa <pauloleitecosta14@gmail.com>
2020-11-20 22:47:35 -0300

    adicionando texto em help e about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,6 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            console.log(body)
             callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
